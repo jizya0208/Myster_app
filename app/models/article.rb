7049cfdccf92a9725d152355cf4c967bc060d4ad
jizya0208 <moridyan606@gmail.com>
2021-07-05 09:34:37 +0000

    [FIX] Articleモデルのimage_idを削除し、新たにArticleImageモデルを生成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :member
   belongs_to :category
-  has_many :article_comments
-  has_many :favorites
+  has_many :article_images, dependent: :destroy
+  has_many :article_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
-  attachment :image
+  accepts_attachments_for :article_images, attachment: :image
   
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_131524) do
+ActiveRecord::Schema.define(version: 2021_07_05_091948) do
 
   create_table "account_histories", force: :cascade do |t|
     t.integer "transaction_id"
@@ -39,11 +39,17 @@ ActiveRecord::Schema.define(version: 2021_07_04_131524) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "article_images", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.integer "member_id"
     t.string "title"
     t.text "body"
-    t.string "image_id"
     t.integer "category_id"
     t.boolean "is_closed"
     t.datetime "created_at", null: false
