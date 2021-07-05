bc9f20b0958953fbbd5f7ebc37f838708cbc94b3
jizya0208 <moridyan606@gmail.com>
2021-07-05 14:13:11 +0000

    [ADD] refileシークレットキー

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9c4bbc56af1b85ce5db37a5a7a6fbe5bb8d8a617230ce647f879d5f1fb424cd2844d67f21d60342e0b8e380eca206b0b1b14a3ea47d30df5a3e7c5c037fec974'
