cec2aae6d575c299ce742cd311d232062056864f
jizya0208 <moridyan606@gmail.com>
2021-07-07 00:31:19 +0000

    [ADD] コメント機能

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,2 +1,26 @@
 class ArticleCommentsController < ApplicationController
+  before_action :authenticate_member!
+
+  def create
+    @article = Article.find(params[:article_id])
+    @article_comment = @article.article_comments.new(article_comment_params)
+    @article_comment.member_id = current_member.id
+    unless @article_comment.save
+      render 'error'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    # 結びつく投稿とコメントIDから抽出
+    article_comment = @article.article_comments.find_by(params[:id])
+    article_comment.destroy
+  end
+  
+
+  private
+
+  def article_comment_params
+    params.require(:article_comment).permit(:body, :image)
+  end
 end
