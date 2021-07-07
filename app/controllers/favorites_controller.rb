790b48521d6693ea807ca1b86f84a459420c5b8c
jizya0208 <moridyan606@gmail.com>
2021-07-07 00:33:14 +0000

    [ADD] お気に入り機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_member!
+
+  def create
+    @article = Article.find(params[:article_id])
+    favorite = @article.favorites.new(member_id: current_member.id)
+    favorite.save
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    favorite = @article.favorites.find_by(member_id: current_member.id)
+    favorite.destroy
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,13 @@ class Article < ApplicationRecord
   has_many :favorites, dependent: :destroy
   
   accepts_attachments_for :article_images, attachment: :image
-  
+
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+  
+  
+  # 既にお気に入り済みか真偽値で返すメソッド
+  def favorited_by?(member)
+    favorites.where(member_id: member.id).exists?
+  end
 end
