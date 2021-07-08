29b3b33bc6a0a9ca89d728e3ec6eba61e5237a0c
jizya0208 <moridyan606@gmail.com>
2021-07-08 09:27:37 +0000

    [UPDATE] 新規登録・ログイン時の許可カラム

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
+  # 新規登録、ログイン時にそれぞれ許可するキーを設定
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :integer, :age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :integer, :age] )
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name] )                         
   end
 end
 
