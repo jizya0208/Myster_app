3b963555a12f12c339aa1e6aed126622c548c9cb
jizya0208 <moridyan606@gmail.com>
2021-07-11 13:33:53 +0000

    [ADD] ルーティング、コントローラにコメントアウト加筆

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  protected
-  # 新規登録、ログイン時にそれぞれ許可するキーを設定
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :integer, :age] )
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name] )                         
+
+  private
+  def after_sign_in_path_for(resource)  #ログイン後の遷移ページをマイページを指定
+    member_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)  #ログアウト後の遷移ページをルートパスを指定
+    root_path
+  end
+
+  def configure_permitted_parameters     #新規登録、ログイン時にそれぞれ許可するキーを設定
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email] )
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name] )
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'homes/about'
-  get "search_in_shares" => "homes#search_in_shares"
-  get "search_in_inquiries" => "homes#search_in_inquiries"
+  get 'search_in_shares' => 'homes#search_in_shares'      
+  get 'search_in_inquiries' => 'homes#search_in_inquiries'
+  get 'article/tag/:name' => 'articles#tag'
 
   #「会員」
+  get 'members/unsubscribe' => 'members#unsubscribe', as: 'confirm_unsubscribe'
+  patch 'members/withdraw' => 'members#withdraw', as: 'withdraw_member'
   devise_for :members, controllers: {
     sessions: 'members/sessions',
     registrations: 'members/registrations'
@@ -22,7 +25,7 @@ Rails.application.routes.draw do
   resources :transactions, only: [:create]
 
   #「投稿」とそれに紐づく「お気に入り」「コメント」「星評価」
-  get 'articles/inquirings' => "articles#inquire", as: 'inquire'
+  get 'articles/inquirings' => 'articles#inquire', as: 'inquire'
   resources :articles, except: [:edit] do
     resources :article_images
     resource  :favorites, only: [:create, :destroy]
