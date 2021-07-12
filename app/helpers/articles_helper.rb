8b0ba7a842a1900f4cf776f1671677d519c2eead
jizya0208 <moridyan606@gmail.com>
2021-07-12 01:03:51 +0000

    [ADD] タグ検索結果表示用のヘルパーメソッドを追加

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+  # 文字列.gsub(/正規表現/, 正規表現に該当した箇所を置換した後の文字列)
+  # html_safeは、対象の文字列が安全であるとマーキングするメソッド。
+  def render_with_tags(body)
+    body.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/article/tag/#{word.delete("#")}"}.html_safe
+  end 
 end
