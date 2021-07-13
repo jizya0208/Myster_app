2c10e30d76175ea7922aaf894d73c011e9cbe393
jizya0208 <moridyan606@gmail.com>
2021-07-13 11:31:32 +0000

    [ADD] 長い文字列の一部だけ表示するヘルパーメソッド

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,4 +4,17 @@ module ArticlesHelper
   def render_with_tags(body)
     body.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/article/tag/#{word.delete("#")}"}.html_safe
   end 
+  
+  
+  def cut_off(text, len) #text:カットしたい対象の文字列,len:字数
+    if text != nil
+      if text.length < len #字数制限以下ならそのまま返す
+        text
+      else
+        text.scan(/^.{#{len}}/m)[0] + "…" # ^ => 直後の文字が行頭にある場合にマッチ . => 任意の一文字　{n} => 直前の文字の桁数(len)を指定  m(オプション) => 改行にもマッチ
+      end
+    else
+      ''
+    end
+  end
 end
