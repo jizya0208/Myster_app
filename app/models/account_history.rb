e3ae9955c4d3cbe1134edfef759f4f4cb1cdfa94
jizya0208 <moridyan606@gmail.com>
2021-07-16 08:10:29 +0000

    [ADD]  バリデーション：取引は常に1ポイント以上

--- app/models/account_history.rb
+++ app/models/account_history.rb
@@ -2,9 +2,10 @@ class AccountHistory < ApplicationRecord
   belongs_to :account
   belongs_to :transaction_number
   belongs_to :transaction_type
-  
+
   validates :amount, presence: true
   validates :remark, presence: true
-  #残高は0ポイント以上
+  #残高は0ポイント以上、取引は1ポイント以上
   validates :balance, numericality: {greater_than_or_equal_to: 0}
+  validates :amount, numericality: {greater_than_or_equal_to: 1}
 end
