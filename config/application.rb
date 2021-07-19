2fb572626ab96cdb803b24ef844f656fec44a3ea
jizya0208 <moridyan606@gmail.com>
2021-07-19 07:28:30 +0000

    [UPDATE] タイムゾーンを日本時間に

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Myster
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja #devise日本語化のための記述
+    config.time_zone = 'Tokyo' #タイムゾーンを日本時間にするための
   end
 end
