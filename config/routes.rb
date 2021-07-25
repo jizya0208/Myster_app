e80fc97aba11411a43019f4efdbba1867973ea2f
jizya0208 <moridyan606@gmail.com>
2021-07-25 09:55:32 +0000

    [UPDATE] aboutページ削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'contact/confirm'
   get 'contact/thanks'
   root 'homes#top'
-  get 'homes/about'
   get 'search_in_shares' => 'homes#search_in_shares'
   get 'search_in_inquiries' => 'homes#search_in_inquiries'
   get '/search' => 'search#search'
@@ -15,7 +14,7 @@ Rails.application.routes.draw do
   end
   get 'members/unsubscribe' => 'members#unsubscribe', as: 'confirm_unsubscribe'
   patch 'members/withdraw' => 'members#withdraw', as: 'withdraw_member'
-  
+
   devise_for :members, controllers: {
     sessions: 'members/sessions',
     registrations: 'members/registrations'
@@ -45,7 +44,7 @@ Rails.application.routes.draw do
   # 通知
   resources :notifications, only: :index
   delete 'destroy_all_members_notifications' => 'notifications#destroy_all'
-  
+
   # 問合せ
   get   'contacts' => 'contact#index' # 入力画面
   post  'contact/confirm' => 'contact#confirm', as: 'confirm'   # 確認画面
