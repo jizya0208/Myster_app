bee5b1c67f41a064421a61037b52c7c115fe3762
jizya0208 <moridyan606@gmail.com>
2021-08-12 10:55:03 +0000

    コメント返信 自己結合関連付け

--- app/models/article_comment.rb
+++ app/models/article_comment.rb
@@ -1,8 +1,10 @@
 class ArticleComment < ApplicationRecord
   belongs_to :member
   belongs_to :article
+  belongs_to :parent, class_name: "ArticleComment", optional: true # 自己の親コメントをparentと名付け、class_nameでコメントテーブル参照を指定。返信コメントでない通常コメントも保存したいので optional: true とし、nilを許容。(Rails5ではデフォルト required: trueとなっている)
   has_many :ratings
   has_many :notifications, dependent: :destroy
+  has_many :replies, class_name: "ArticleComment", foreign_key: :parent_id # 自己の子供をreplyと名付け、class_name, foreign_keyとともに記述。上記の:parentと合わせて自己結合関連付けが出来る。
 
   attachment :image
 
