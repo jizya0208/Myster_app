1e42a078615c12007c36416cbc38cb2a3bf25ef3
jizya0208 <moridyan606@gmail.com>
2021-08-24 07:57:42 +0000

    [UPDATE] コメント星評価のリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
   def current_member_has?(instance)
-    member_signed_in? && current_member == instance.member # 会員がログイン且つインスタンスに紐付いた会員が同一か
-  end
-  
-  def other_member?(member)
-    member != current_member # 会員がログイン且つインスタンスに紐付いた会員が同一か
+    member_signed_in? && (current_member == instance.member) # 会員がログイン且つインスタンスに紐付いた会員が同一か
   end
 end
--- app/helpers/article_comments_helper.rb
+++ app/helpers/article_comments_helper.rb
@@ -1,2 +1,5 @@
 module ArticleCommentsHelper
+  def answer_from_other_member(article, comment)
+    !(article.is_closed.nil?) && (current_member != comment.member)
+  end
 end
