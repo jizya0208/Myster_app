2dba935f2e470822810fafad0fc1779a380877e1
jizya0208 <moridyan606@gmail.com>
2021-08-30 08:39:17 +0000

    [PATCH] 円グラフ、多い順に並び替え

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,9 +51,9 @@ class ArticlesController < ApplicationController
     #-------------------グラフ表示用の変数 (世代・男女別) --------------------------
     favorite_member_attributes = Member.where(id: @article.favorites.map(&:member_id)) # 投稿に紐づくお気に入りから、その会員情報を取得する
     array = favorite_member_attributes.map(&:classify)                                 # 世代･男女別(30代前半女性etc..)に変換した上で、新たな配列を生成
-    @attribute_ratio = array.group_by(&:itself).map{ |key,value| [key, value.count] }.sort.to_h # 上記の配列を評価して、同じ区分の数をバリューに入れハッシュ化。sortメソッドで多い順に並ぶ
+    @attribute_ratio = p array.group_by(&:itself).map{ |key,value| [key, value.size] }.sort{ |a, b| b[1] <=> a[1] }.to_h # 上記を評価し同じ区分の数をvalueに入れ、当該valueで降順にソートし、ハッシュ化
     @attribute_ratio.each do |k, v|
-      ratio = (v * 100).to_f / favorite_member_attributes.count   # 区分ごとのデータ数割合を%表示
+      ratio = (v * 100).to_f / favorite_member_attributes.size   # 区分ごとのデータ数割合を%表示
       @attribute_ratio[k] = ratio.round(1)
     end
   end
